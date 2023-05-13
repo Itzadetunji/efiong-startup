02f9a1d89bae85f34321e21cc3e24a77417b1ff5
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2023-05-13 16:59:37 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import React from "react";
+
+const Index: NextPage = () => {
+	return (
+		<section className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl">
+			Initial Commit
+		</section>
+	);
+};
+
+export default Index;
